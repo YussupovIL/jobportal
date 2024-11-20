b06692a40eff29c8ac322fde328c742f5f2867b6
iliyas <iliyas.yussupov@gmail.com>
2024-11-20 12:59:21 +0100

    cv image uplaod to job candidate profile

--- src/main/java/com/iliyas/jobportal/jobportal/controller/JobSeekerProfileController.java
+++ src/main/java/com/iliyas/jobportal/jobportal/controller/JobSeekerProfileController.java
@@ -5,6 +5,7 @@ import com.iliyas.jobportal.jobportal.entity.Skills;
 import com.iliyas.jobportal.jobportal.entity.Users;
 import com.iliyas.jobportal.jobportal.repository.UsersRepository;
 import com.iliyas.jobportal.jobportal.services.JobSeekerProfileService;
+import com.iliyas.jobportal.jobportal.util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,11 +13,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -58,4 +65,54 @@ public class JobSeekerProfileController {
 
         return "job-seeker-profile";
     }
+
+    @PostMapping("/addNew")
+    public String addNew(JobSeekerProfile jobSeekerProfile, @RequestParam("image")MultipartFile image, @RequestParam("pdf")MultipartFile pdf, Model model ){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            Users user = usersRepository.findByEmail(authentication.getName()).orElseThrow(()-> new UsernameNotFoundException("user not found"));
+            jobSeekerProfile.setUserId(user);
+            jobSeekerProfile.setUserAccountId(user.getUserId());
+        }
+
+        List<Skills> skillsList = new ArrayList<>();
+        model.addAttribute("profile", jobSeekerProfile);
+        model.addAttribute("skills", skillsList);
+
+        for(Skills skills : jobSeekerProfile.getSkills()){
+            skills.setJobSeekerProfile(jobSeekerProfile);
+        }
+
+        String imageName = "";
+        String resumeName ="";
+
+        if(!Objects.equals(image.getOriginalFilename(), "")){
+            imageName = StringUtils.cleanPath(Objects.requireNonNull(image.getOriginalFilename()));
+            jobSeekerProfile.setProfilePhoto(imageName);
+        }
+
+        if(!Objects.equals(pdf.getOriginalFilename(), "")){
+            resumeName = StringUtils.cleanPath(Objects.requireNonNull(pdf.getOriginalFilename()));
+            jobSeekerProfile.setResume(resumeName);
+        }
+        JobSeekerProfile seekerProfile = jobSeekerProfileService.addNew(jobSeekerProfile);
+
+        try {
+            String uploadDirectory = "photos/candidate/" + jobSeekerProfile.getUserAccountId();
+            if(!Objects.equals(image.getOriginalFilename(),"")){
+                FileUploadUtil.saveFile(uploadDirectory, imageName, image);
+            }
+            if(!Objects.equals(image.getOriginalFilename(),"")){
+                FileUploadUtil.saveFile(uploadDirectory, resumeName, pdf);
+            }
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+
+        return  "redirect:/dashboard/";
+    }
+
 }
